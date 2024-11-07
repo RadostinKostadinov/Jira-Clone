9f3a44fbe7e02d2e3c21882a6e34280063680935
Radostin Kostadinov <radostin.kostadinov@car-digital.eu>
2024-11-07 10:23:54 +0200

    Add login & register endpoints(BE)  & hooks for them(FE)

--- src/features/auth/components/sign-up-card.tsx
+++ src/features/auth/components/sign-up-card.tsx
@@ -10,17 +10,14 @@ import { useForm } from 'react-hook-form';
 import {z} from "zod";
 import {zodResolver} from "@hookform/resolvers/zod";
 import {Form, FormControl, FormField, FormItem, FormMessage} from "@/components/ui/form";
-
-const formSchema = z.object({
-    name: z.string().trim().min(1, "Required"),
-    email: z.string().email(),
-    password: z.string().min(8, "Minimum of 8 characters required.")
-})
-
+import {registerSchema} from "@/features/auth/schemas";
+import {useRegister} from "@/features/auth/api/use-register";
 
 export const SignUpCard = () => {
-    const form = useForm<z.infer<typeof formSchema>>({
-        resolver: zodResolver(formSchema),
+    const { mutate } = useRegister();
+
+    const form = useForm<z.infer<typeof registerSchema>>({
+        resolver: zodResolver(registerSchema),
         defaultValues: {
             name: "",
             email: "",
@@ -28,8 +25,8 @@ export const SignUpCard = () => {
         }
     });
 
-    const onSubmit = (values: z.infer<typeof formSchema>) => {
-        console.log(values);
+    const onSubmit = (values: z.infer<typeof registerSchema>) => {
+        mutate({json: values});
     }
 
     return (
